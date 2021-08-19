1ecc19da8100e815e450fafc359dfd62bdec4b79
Mohammadreza <mr.anjedani@gmail.com>
2021-08-19 17:31:41 +0430

    last of partial view

---
+++ DevCard_Livici/Models/IndexModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevCard_Livici.Models
+{
+    public class IndexModel
+    {
+
+        public List<Project> Projects { get; set; }
+        public List<Article> Articles { get; set; }
+    }
+}
