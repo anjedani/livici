3791eb6815ba331ccf10aff72d6b239959358bb2
Mohammadreza <mr.anjedani@gmail.com>
2021-08-25 17:07:43 +0430

    Article and Project  and headr and footer partial view

---
+++ DevCard_Livici/ViewComponents/ArticlesViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using DevCard_Livici.Models;
+using Microsoft.AspNetCore.Mvc;
+
+using System.Collections.Generic;
+
+namespace DevCard_Livici.ViewComponents
+{
+    public class ArticlesViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+
+
+            var maghale = new List<Article>
+            {
+            new Article(1,"jhdjg","1دسکریپشن","blog-post-thumb-card-1.jpg"),
+            new Article(2,"gh","دسکریپشن2","blog-post-thumb-card-2.jpg"),
+            new Article(3,"hh","3دسکریپشن","blog-post-thumb-card-3.jpg"),
+            new Article(4,"jjj","4دسکریپشن","blog-post-thumb-card-4.jpg")
+
+            };
+             return View("Maghalat",maghale);
+        }
+    }
+}
---
+++ DevCard_Livici/ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using DevCard_Livici.Models;
+using Microsoft.AspNetCore.Mvc;
+
+using System.Collections.Generic;
+
+namespace DevCard_Livici.ViewComponents
+{
+    public class ProjectsViewComponent : ViewComponent
+
+    {
+            public IViewComponentResult Invoke()
+            {
+
+                var halim = new List<Project>
+                {
+
+                    new Project(1,"googooli","11111","project-1.jpg","dfsdfsdf"),
+                    new Project(2,"googooli2","22222","project-2.jpg","dfsdfsdf"),
+                    new Project(3,"googooli3","333333","project-3.jpg","dfsdfsdf"),
+                    new Project(4,"googooli4","project-4","project-4.jpg","dfsdfsdf")
+                };
+            
+
+             return View("jakesh",halim);
+
+            }
+
+    }
+}
